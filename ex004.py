8a29701ba24ed770b2b219a146d5999747869bff
vinibispo <vini.bispo015@gmail.com>
2019-06-07 08:34:07 -0300

    exercise 4 complete

--- ex004.py
+++ ex004.py
@@ -1,9 +1,12 @@
 def palindrome(n):
+    l = list()
     for i in range(10 ** (n-1), 10 ** n):
         for j in range(10 ** (n-1), 10 ** n):
             s = str(i * j)
-            # if (s[0:n] == s[n:-1]):
-            #     print(s)
-            print(s)
+            # 9090
+            if s == s[::-1]:
+                l.append(i * j)
+    return max(l)
+print(palindrome(3))
 
-palindrome(2)
+    # nT5UWeVQDk3nA8?uJX{Weu}u
