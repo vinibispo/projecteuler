24e9d2e95da8bce1e50bc6ff50ddd438554f0058
vinibispo <vini.bispo015@gmail.com>
2019-06-07 20:18:30 -0300

    ex005 complete

--- ex005.py
+++ ex005.py
@@ -1,4 +1,18 @@
-def smallest(number):
-    m = 1
-    for i in range(1, number):
-        
+def getBiggestNumberThatIsEvenlyDivisible(limitNumber):
+  testNumber = 2
+  while True:
+    i = 2
+    while True:
+      if testNumber % i != 0:
+        break
+      if i == limitNumber:
+        return testNumber
+      i += 1
+    testNumber += 1
+    
+  
+  return True
+
+
+
+print(getBiggestNumberThatIsEvenlyDivisible(20))
