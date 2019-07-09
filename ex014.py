dcade963e910fa78d64e7a73f9086dacd7a53100
vinibispo <vini.bispo015@gmail.com>
2019-07-09 10:13:40 -0300

    ex014 almost complete and upgrading memoize

--- ex014.py
+++ ex014.py
@@ -1,1 +1,17 @@
-from memoize
+def collatz_sequence(n):
+    c = 0
+    while n > 1:
+        c += 1
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n += 2 * n + 1
+    return c
+n = 10 ** 6
+largest = 0
+for i in range(1, n + 1):
+    c = collatz_sequence(i)
+    if c > largest:
+        largest = c
+        print(largest)
+print(largest)
--- memoize.py
+++ memoize.py
@@ -1,7 +1,10 @@
-def memoize(f):
-    memo = {}
-    def helper(x):
-        if x not in memo:
-            memo[x] = f(x)
-        return memo[x]
-    return helper
+class Memoize:
+
+    def __init__(self, fn):
+        self.fn = fn
+        self.memo = {}
+
+    def __call__(self, *args):
+        if args not in self.memo:
+	        self.memo[args] = self.fn(*args)
+        return self.memo[args]
